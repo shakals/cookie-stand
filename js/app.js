3c97385a37ec1f07e804b0d249710c3ac1437b16
Shan Kalyanasundaram <shakals@amazon.com>
2019-03-02 20:34:44 -0800

    Enhanced js to calculate cookies per hour and also added basic structure for sales.html

--- js/app.js
+++ js/app.js
@@ -18,6 +18,26 @@ function generateRandomCustomerCount (locationOpenHour, locationCloseHour, minCu
 }
 
 
+//This function accepts 2 inputs: Average count of cookies for the store and an array of arrays containing customer count per hour for each hour between Store open and close time.
+//Then, it generates random number of customers for each hour.
+function calculateCookieCount (avgCookiesPerCustomer, customerArray) {
+  //Create an array to store the hour (first element) and calculated cookie count each hour (second element). This will later become an array of arrays in subsequent steps.
+  var cookiesPerHour = [];
+
+  //Loop through the customer count Array to calculate cookie count each hour and store the results in cookiesPerHour array (another array of arrays)
+  for (var i = 0; i < customerArray.length; i++){    
+    cookiesPerHour[i] = [customerArray[i][0], (customerArray[i][1] * avgCookiesPerCustomer)];
+    console.log('cookiesPerHour[' + i + ']' + '[0] =' + cookiesPerHour[i][0] + ' -- [' + i + ']' + '[1] =' + cookiesPerHour[i][1]);    
+  }
+  return cookiesPerHour;
+}
+
+/*function displaySalesProjections(ulID){
+  var ulStore = document.getElementById(ulID);
+
+ // for (var i = 0; i < )
+}*/
+
 //1st and Pike Place Store
 var FirstAndPikeStore = {
   locationName: '1st and Pike',
@@ -27,13 +47,12 @@ var FirstAndPikeStore = {
   maxHourlyCustomers: 65,
   avgCookiesPerCustomer: 6.3,
   customersPerHour: [],
-  getCustomersPerHour: function(){
-    this.customersPerHour = generateRandomCustomerCount(this.locationOpenHour, this.locationCloseHour, this.minHourlyCustomers, this.maxHourlyCustomers);
-  }
+  cookiesPerHour: [],
+  totalCookies: 0
 };
 
-//console.log('result=', FirstAndPikeStore.getCustomersPerHour());
-//console.log('result=', generateRandomCustomerCount(FirstAndPikeStore.locationOpenHour, FirstAndPikeStore.locationCloseHour, FirstAndPikeStore.minHourlyCustomers, FirstAndPikeStore.maxHourlyCustomers));
-FirstAndPikeStore.getCustomersPerHour();
-console.log('FirstAndPikeStore object prop =', FirstAndPikeStore.customersPerHour[13][1]);
+FirstAndPikeStore.customersPerHour = generateRandomCustomerCount(FirstAndPikeStore.locationOpenHour, FirstAndPikeStore.locationCloseHour, FirstAndPikeStore.minHourlyCustomers, FirstAndPikeStore.maxHourlyCustomers);
+console.log('FirstAndPikeStore customersPerHour array element 13,1 =', FirstAndPikeStore.customersPerHour[13][1]);
 
+FirstAndPikeStore.cookiesPerHour = calculateCookieCount(FirstAndPikeStore.avgCookiesPerCustomer, FirstAndPikeStore.customersPerHour);
+console.log('FirstAndPikeStore cookiesPerHour array element 5,1 =', FirstAndPikeStore.cookiesPerHour[5][1]);
