af88a8673f1e44e60a3f90d1420aa7fe94a6ebfc
Shan Kalyanasundaram <shakals@amazon.com>
2019-03-03 20:18:02 -0800

    Changed array of arrays logic to array of objects to hold cookie data and populated the list in html for 1st and Pike

--- js/app.js
+++ js/app.js
@@ -1,58 +1,58 @@
+'use strict';
 
-//This function accepts a time range (Opening and Closing time of store in hours) and minimum and maximum limit for number of customers during any hour for that store.
-//Then, it generates random number of customers for each hour.
-function generateRandomCustomerCount (locationOpenHour, locationCloseHour, minCustomers, maxCustomers) {
-  //Create an array to store the hour (first element) and respective customer count each hour (second element). This will later become an array of arrays in subsequent steps.
-  var customersPerHour = [];
+/* This function accepts a time range (Opening and Closing time of store in hours), minimum and maximum limit for number of customers during any hour for that store and Average Cookies sold per customer.
+   Then, it generates random number of customers and calculates cookie count for each hour. */
+function calcCookieCount(startHour, endHour, minCustomers, maxCustomers, avgCookiesPerCustomer){
+  // Declare an array that will later become an array of objects containing Customer and Cookie count for each hour
+  var arr = [];
   var arrIndex = 0;
 
-  //Loop through the hours to generate random number of customers for each hour
-  for (var i = locationOpenHour; i <= locationCloseHour; i++){
-    // 'customersPerHour' becomes an array of arrays in the next step (emulating a 2D array to store hour and customer count values for every hour from opening time till closing time of store)
-    // The Math function will generate random numbers inclusive of both boundaries.
-    customersPerHour[arrIndex] = [i, Math.floor(Math.random() * (maxCustomers - minCustomers + 1)) + minCustomers];
-    console.log('customersPerHour[' + arrIndex + ']' + '[0] =' + customersPerHour[arrIndex][0] + ' -- [' + arrIndex + ']' + '[1] =' + customersPerHour[arrIndex][1]);
+  for (var i = startHour + 1; i <= endHour; i++){
+    // The following statement will generate random numbers inclusive of both boundaries.
+    var tempCustCount = Math.floor(Math.random() * (maxCustomers - minCustomers + 1)) + minCustomers;
+    arr[arrIndex] = {      
+      hour: ((i <= 12) ? i:i%12) + ((i < 12) ? ' AM':' PM'), // Assign the hour (in 12 hour format) to this property
+      customerCount: tempCustCount, // Random customer count generated above
+      cookieCount: Math.ceil(tempCustCount * avgCookiesPerCustomer) // Cookie count is calculated by multiplying Customer Count with Average cookies sold per customer and then rounding upwards to nearest integer
+    };
     arrIndex++;
-  }
-  return customersPerHour;
+  }  
+  return arr;
 }
 
+/* This function dynamically generates and populates list items (hourly cookie count) for the Unordered list (Store) passed as parameter */
+function displaySalesProjections(ulID, cookieData){
+  var ulStore = document.getElementById(ulID); //Unordered List for the Store
 
-//This function accepts 2 inputs: Average count of cookies for the store and an array of arrays containing customer count per hour for each hour between Store open and close time.
-//Then, it generates random number of customers for each hour.
-function calculateCookieCount (avgCookiesPerCustomer, customerArray) {
-  //Create an array to store the hour (first element) and calculated cookie count each hour (second element). This will later become an array of arrays in subsequent steps.
-  var cookiesPerHour = [];
-
-  //Loop through the customer count Array to calculate cookie count each hour and store the results in cookiesPerHour array (another array of arrays)
-  for (var i = 0; i < customerArray.length; i++){    
-    cookiesPerHour[i] = [customerArray[i][0], (customerArray[i][1] * avgCookiesPerCustomer)];
-    console.log('cookiesPerHour[' + i + ']' + '[0] =' + cookiesPerHour[i][0] + ' -- [' + i + ']' + '[1] =' + cookiesPerHour[i][1]);    
+  for (var i = 0; i < cookieData.length; i++) {
+    var liHourlyData = document.createElement('li');
+    liHourlyData.textContent = cookieData[i].hour + ': ' + cookieData[i].cookieCount + ' cookies'; //Populate hourly cookie data in the new list item
+    ulStore.append(liHourlyData);
   }
-  return cookiesPerHour;
 }
 
-/*function displaySalesProjections(ulID){
-  var ulStore = document.getElementById(ulID);
-
- // for (var i = 0; i < )
-}*/
 
 //1st and Pike Place Store
+var ulID = 'FirstAndPike';
 var FirstAndPikeStore = {
   locationName: '1st and Pike',
   locationOpenHour: 6,
   locationCloseHour: 20,
   minHourlyCustomers: 23,
   maxHourlyCustomers: 65,
-  avgCookiesPerCustomer: 6.3,
-  customersPerHour: [],
-  cookiesPerHour: [],
+  avgCookiesPerCustomer: 6.3,  
+  hourlyCustomerCookies:[],  
+  calcCookies:function(){
+    this.hourlyCustomerCookies = calcCookieCount(this.locationOpenHour, this.locationCloseHour, this.minHourlyCustomers, this.maxHourlyCustomers, this.avgCookiesPerCustomer);
+  },
   totalCookies: 0
 };
 
-FirstAndPikeStore.customersPerHour = generateRandomCustomerCount(FirstAndPikeStore.locationOpenHour, FirstAndPikeStore.locationCloseHour, FirstAndPikeStore.minHourlyCustomers, FirstAndPikeStore.maxHourlyCustomers);
-console.log('FirstAndPikeStore customersPerHour array element 13,1 =', FirstAndPikeStore.customersPerHour[13][1]);
+FirstAndPikeStore.calcCookies();
+console.log('Average Cookies Per Customer =', FirstAndPikeStore.avgCookiesPerCustomer);
+console.log('hourlyCustomerCookies Array =', FirstAndPikeStore.hourlyCustomerCookies);
 
-FirstAndPikeStore.cookiesPerHour = calculateCookieCount(FirstAndPikeStore.avgCookiesPerCustomer, FirstAndPikeStore.customersPerHour);
-console.log('FirstAndPikeStore cookiesPerHour array element 5,1 =', FirstAndPikeStore.cookiesPerHour[5][1]);
+/* Populate data in sales.html */
+displaySalesProjections(ulID, FirstAndPikeStore.hourlyCustomerCookies);
+
+
