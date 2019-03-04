82a8bdececc10c02a926345dfcf76a65cf717ee1
Shan Kalyanasundaram <shakals@amazon.com>
2019-03-03 23:26:35 -0800

    Completed code for SeaTac Airport

--- js/app.js
+++ js/app.js
@@ -10,29 +10,34 @@ function calcCookieCount(startHour, endHour, minCustomers, maxCustomers, avgCook
   for (var i = startHour + 1; i <= endHour; i++){
     // The following statement will generate random numbers inclusive of both boundaries.
     var tempCustCount = Math.floor(Math.random() * (maxCustomers - minCustomers + 1)) + minCustomers;
-    arr[arrIndex] = {      
+    arr[arrIndex] = {
       hour: ((i <= 12) ? i:i%12) + ((i < 12) ? ' AM':' PM'), // Assign the hour (in 12 hour format) to this property
       customerCount: tempCustCount, // Random customer count generated above
       cookieCount: Math.ceil(tempCustCount * avgCookiesPerCustomer) // Cookie count is calculated by multiplying Customer Count with Average cookies sold per customer and then rounding upwards to nearest integer
     };
     arrIndex++;
-  }  
+  }
   return arr;
 }
 
 /* This function dynamically generates and populates list items (hourly cookie count) for the Unordered list (Store) passed as parameter */
-function displaySalesProjections(ulID, cookieData){
+function displaySalesProjections(ulID, objCookieData){
   var ulStore = document.getElementById(ulID); //Unordered List for the Store
+  var liHourlyData;
 
-  for (var i = 0; i < cookieData.length; i++) {
-    var liHourlyData = document.createElement('li');
-    liHourlyData.textContent = cookieData[i].hour + ': ' + cookieData[i].cookieCount + ' cookies'; //Populate hourly cookie data in the new list item
+  for (var i = 0; i < objCookieData.hourlyCustomerCookies.length; i++) {
+    liHourlyData = document.createElement('li');
+    liHourlyData.textContent = objCookieData.hourlyCustomerCookies[i].hour + ': ' + objCookieData.hourlyCustomerCookies[i].cookieCount + ' cookies'; //Populate hourly cookie data in the new list item
     ulStore.append(liHourlyData);
   }
+
+  liHourlyData = document.createElement('li');
+  liHourlyData.textContent = 'Total: ' + objCookieData.totalCookies + ' cookies'; //Populate total cookie count at the end of the list
+  ulStore.append(liHourlyData);
 }
 
 
-//1st and Pike Place Store
+/* 1st and Pike Place Store */
 var ulID = 'FirstAndPike';
 var FirstAndPikeStore = {
   locationName: '1st and Pike',
@@ -44,6 +49,10 @@ var FirstAndPikeStore = {
   hourlyCustomerCookies:[],
   calcCookies:function(){
     this.hourlyCustomerCookies = calcCookieCount(this.locationOpenHour, this.locationCloseHour, this.minHourlyCustomers, this.maxHourlyCustomers, this.avgCookiesPerCustomer);
+
+    for (var i = 0; i < this.hourlyCustomerCookies.length; i++){
+      this.totalCookies += this.hourlyCustomerCookies[i].cookieCount;
+    }
   },
   totalCookies: 0
 };
@@ -51,8 +60,37 @@ var FirstAndPikeStore = {
 FirstAndPikeStore.calcCookies();
 console.log('FirstAndPikeStore Average Cookies Per Customer =', FirstAndPikeStore.avgCookiesPerCustomer);
 console.log('FirstAndPikeStore hourlyCustomerCookies Array =', FirstAndPikeStore.hourlyCustomerCookies);
+console.log('FirstAndPikeStore Total Cookies =', FirstAndPikeStore.totalCookies);
 
-/* Populate data in sales.html */
-displaySalesProjections(ulID, FirstAndPikeStore.hourlyCustomerCookies);
+// Populate data in sales.html
+displaySalesProjections(ulID, FirstAndPikeStore);
 
+
+
+/* SeaTac Airport */
+ulID = 'SeaTacAirport';
+var SeaTacAirport = {
+  locationName: 'SeaTac Airport',
+  locationOpenHour: 6,
+  locationCloseHour: 20,
+  minHourlyCustomers: 3,
+  maxHourlyCustomers: 24,
+  avgCookiesPerCustomer: 1.2,
+  hourlyCustomerCookies:[],
+  calcCookies:function(){
+    this.hourlyCustomerCookies = calcCookieCount(this.locationOpenHour, this.locationCloseHour, this.minHourlyCustomers, this.maxHourlyCustomers, this.avgCookiesPerCustomer);
+
+    for (var i = 0; i < this.hourlyCustomerCookies.length; i++){
+      this.totalCookies += this.hourlyCustomerCookies[i].cookieCount;
+    }
+  },
+  totalCookies: 0
+};
+
+SeaTacAirport.calcCookies();
+console.log('SeaTacAirport Average Cookies Per Customer =', SeaTacAirport.avgCookiesPerCustomer);
+console.log('SeaTacAirport hourlyCustomerCookies Array =', SeaTacAirport.hourlyCustomerCookies);
+
+// Populate data in sales.html
+displaySalesProjections(ulID, SeaTacAirport);
 
